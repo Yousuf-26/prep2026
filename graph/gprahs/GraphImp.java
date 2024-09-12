c8244c4d752ce825ba5190755ca015b6a6e55586
Yousuf-26 <yousuf.cloud.26@gmail.com>
2024-09-11 19:02:27 -0700

    added weight attribute

--- graph/gprahs/GraphImp.java
+++ graph/gprahs/GraphImp.java
@@ -16,7 +16,9 @@ public class GraphImp
        
        for(int i = 0; i<graph[2].size(); i++ ){
         Edge e = graph[2].get(i);
-        System.out.print(e.dest+" ");
+        System.out.println(" Neighbor of 2: "+e.dest);
+        System.out.println(" Associated weight of 2: "+e.wt);
+        System.out.println("----------------------------");
         }
        
     
@@ -25,10 +27,12 @@ public class GraphImp
     static class Edge{
     int src;
     int dest;
+    int wt;
     
-    public Edge(int s, int d){
+    public Edge(int s, int d, int wt){
        this.src = s;
        this.dest =d;
+       this.wt = wt;
     }
     }
     
@@ -38,17 +42,17 @@ public class GraphImp
         graph[i] = new ArrayList<Edge>();
         }
         
-        graph[0].add(new Edge(0,2));
+        graph[0].add(new Edge(0,2,2));
         
-        graph[1].add(new Edge(1,2));
-        graph[1].add(new Edge(1,3));
+        graph[1].add(new Edge(1,2,10));
+        graph[1].add(new Edge(1,3,0));
         
-        graph[2].add(new Edge(2,0));
-        graph[2].add(new Edge(2,1));
-        graph[2].add(new Edge(2,3));
+        graph[2].add(new Edge(2,0,2));
+        graph[2].add(new Edge(2,1,10));
+        graph[2].add(new Edge(2,3,-1));
         
-        graph[3].add(new Edge(3,1));
-        graph[3].add(new Edge(3,2));
+        graph[3].add(new Edge(3,1,0));
+        graph[3].add(new Edge(3,2,-1));
         
     }
 }
